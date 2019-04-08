ec3fd5dcd6f300f4dae2ba42327405131965a69d
Will Fritts <waf515@gmail.com>
2019-04-08 13:30:54 -0700

    structure built, constructor function added

--- js/app.js
+++ js/app.js
@@ -6,13 +6,21 @@
 
 
 //      Global Variables
-
+var allProducts = [];
 
 //      DOM Variables
 
 
 //      Constructor Functions
-
+function productImage(name){
+  console.log(name, ' is created');
+  this.filepath = `img/${name}.jpg`;
+  this.name = name;
+  this.views = 0;
+  this.votes = 0;
+  allProducts.push(this);
+  console.log(name, ' is pushed to allProducts array');
+}
 
 // ++++++++++++++++++++Define Behaviors/Actions++++++++++++++++++++\\
 
