57860fcf9db28b23724f991675cd3d19b7c81076
Will Fritts <waf515@gmail.com>
2019-04-08 13:22:42 -0700

    set structure of js app

---
+++ js/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// ++++++++++++++++++++DATA++++++++++++++++++++\\
+
+//      App Data
+
+
+//      Global Variables
+
+
+//      DOM Variables
+
+
+//      Constructor Functions
+
+
+// ++++++++++++++++++++Define Behaviors/Actions++++++++++++++++++++\\
+
+//      Function Declarations
+
+//  Helper Functions
+
+//  Core Functionality
+
+//  Event Handlers
+
+
+// ++++++++++++++++++++Executable Code++++++++++++++++++++\\
+
+//      Functions Calls
+
+//      Event listeners
