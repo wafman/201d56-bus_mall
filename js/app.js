1f802607b51ee5f475a3f95cb6515bf29d9ce60b
Will Fritts <waf515@gmail.com>
2019-04-09 21:42:57 -0700

    fixed repeating image bug. created alert to user of remaining votes but is clicked before end survey

--- js/app.js
+++ js/app.js
@@ -10,10 +10,6 @@ var allProducts = [];
 
 var previousArray = [];
 
-var previous1;
-var previous2;
-var previous3;
-
 var totalVotes = 25;
 
 var products = [];
@@ -48,31 +44,13 @@ function ProductImage(name){
 
 //  Helper Functions
 function newCheckProductArray(product){
-  // console.log('checking new check array with .include');
-  if(previousArray.length < 6){
-    if(previousArray.includes(product)){
-      product = Math.floor(Math.random() * allProducts.length);
-      // console.log('cond1 replace', product);
-    }
-  } else {
-    previousArray = previousArray.slice(-6, previousArray.length);
-    if(previousArray.includes(product)){
-      product = Math.floor(Math.random() * allProducts.length);
-      // console.log('cond2 replace', product);
-    }
+  previousArray = previousArray.slice(-6, previousArray.length);
+  if(previousArray.includes(product)){
+    product = Math.floor(Math.random() * allProducts.length);
+    
   }
   return product;
 }
-
-// function checkProductArray(product){
-//   for(var i = 0; i < previousArray.length; i++){
-//     if(product === previousArray[i]){
-//       // console.log('checking previousArray');
-//       product = Math.floor(Math.random() * allProducts.length);
-//     }
-//   }
-//   return product;
-// }
 
 function updateChartArrays() {
   for(var i = 0; i < allProducts.length; i++){
@@ -96,77 +74,6 @@ function showRandom(imageId){
 }
 
 //  Core Functionality
-// function showRandomProduct1(){
-//   // console.log('showRandomProduct called');
-//   var randomImg1 = Math.floor(Math.random() * allProducts.length);
-//   // console.log(randomImg1);
-//   // checkProductArray(randomImg1);
-//   newCheckProductArray();
-//   // while(randomImg1 === previous1 || randomImg1 === previous2 || randomImg1 === previous3){
-//   //   randomImg1 = Math.floor(Math.random() * allProducts.length);
-//   //   // console.log('duplicate found');
-//   //   // console.log('new randimage is ', randomImg1);
-//   // }
-//   previous1 = randomImg1;
-//   // previousArray[0] = randomImg1;
-//   allProducts[randomImg1].views++;
-//   product1.src = allProducts[randomImg1].filepath;
-//   product1.alt = allProducts[randomImg1].name;
-//   product1.title = allProducts[randomImg1].name;
-//   // var product1Vote = allProducts[randomImg1].votes; //new line testing debug of votes not counting
-//   // if(event.target === product1){
-//   //   allProducts[randomImg1].votes++;
-//   // }
-//   return allProducts[randomImg1];
-// }
-
-// function showRandomProduct2(){
-//   // console.log('showRandomProduct called');
-//   var randomImg2 = Math.floor(Math.random() * allProducts.length);
-//   // console.log(randomImg2);
-//   newCheckProductArray();
-//   // checkProductArray(randomImg2);
-//   while(randomImg2 === previous1 || randomImg2 === previous2 || randomImg2 === previous3){
-//     randomImg2 = Math.floor(Math.random() * allProducts.length);
-//     // console.log('duplicate found');
-//     // console.log('new randimage is ', randomImg2);
-//   }
-//   previous2 = randomImg2;
-//   previousArray[1] = randomImg2;
-//   allProducts[randomImg2].views++;
-//   product2.src = allProducts[randomImg2].filepath;
-//   product2.alt = allProducts[randomImg2].name;
-//   product2.title = allProducts[randomImg2].name;
-//   // allProducts[randomImg2].votes; //new line testing debug of votes not counting
-//   // if(event.target === product2){
-//   //   allProducts[randomImg2].votes++;
-//   // }
-
-// }
-
-// function showRandomProduct3(){
-//   // console.log('showRandomProduct called');
-//   var randomImg3 = Math.floor(Math.random() * allProducts.length);
-//   // console.log(randomImg3);
-//   newCheckProductArray();
-//   // checkProductArray(randomImg3);
-//   while(randomImg3 === previous1 || randomImg3 === previous2 || randomImg3 === previous3){
-//     randomImg3 = Math.floor(Math.random() * allProducts.length);
-//     // console.log('duplicate found');
-//     // console.log('new randimage is ', randomImg3);
-//   }
-//   previous1 = randomImg3;
-//   previousArray[2] = randomImg3;
-//   allProducts[randomImg3].views++;
-//   product3.src = allProducts[randomImg3].filepath;
-//   product3.alt = allProducts[randomImg3].name;
-//   product3.title = allProducts[randomImg3].name;
-//   // allProducts[randomImg3].votes; //new line testing debug of votes not counting
-//   // if(event.target === product3){
-//   //   allProducts[randomImg3].votes++;
-//   // }
-
-// }
 
 function renderList(){
   var liEl = document.createElement('li');
@@ -201,9 +108,6 @@ function handleSectionClick(event){
     showRandom(product1);
     showRandom(product2);
     showRandom(product3);
-    // showRandomProduct1();
-    // showRandomProduct2();
-    // showRandomProduct3();
   } else {
     productContainer.removeEventListener('click', handleSectionClick);
     showElement('generateList');
@@ -241,9 +145,7 @@ showTotalVotes();
 showRandom(product1);
 showRandom(product2);
 showRandom(product3);
-// showRandomProduct1();
-// showRandomProduct2();
-// showRandomProduct3();
+
 
 
 
@@ -251,17 +153,26 @@ showRandom(product3);
 productContainer.addEventListener('click', handleSectionClick);
 
 document.getElementById('generateChart').addEventListener('click', function(){
-  drawChart();
-  console.log('chart is drawn');
+  if(voteTally !== 0){
+    alert('You have ' + totalVotes + ' votes left.')
+  } else {
+    drawChart();
+    console.log('chart is drawn');
+  }
 });
 
 document.getElementById('generateList').addEventListener('click', function() {
-  renderList();
+  if(voteTally !== 0){
+    alert('You have ' + totalVotes + ' votes left.')
+  } else {
+    renderList();
+  }
 });
 
 document.getElementById('voteTally').addEventListener('click', function() {
   document.getElementById('voteTally').hidden = true;
 });
+
 
 
 
